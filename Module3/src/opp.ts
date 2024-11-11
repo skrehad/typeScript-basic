b649bcad0c22b66d01024935a40d7ac9d67fc832
rehadhasan <rehadhasan664@gmail.com>
2024-11-12 02:10:51 +0600

    create a new file

--- Module3/src/opp.ts
+++ Module3/src/opp.ts
@@ -1,11 +1,1 @@
-{
-function getFirstElement<T>(arr: T[]): T {
-    return arr[0];
-  }
-  
-  const numbers = [10, 20, 30];
-  const strings = ["apple", "banana", "cherry"];
-  
-//   console.log(getFirstElement(numbers)); // 10
-  console.log(getFirstElement(strings)); // "apple"
-  }
+console.log("Hello World");
